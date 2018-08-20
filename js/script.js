d2809ca8e556ca9573afa3809061dfc7ce52c321
Szymon Miks <miks.szymon@gmail.com>
2018-08-20 20:56:15 +0200

    change img size, repair navbar on mobile, refine project section, first steps in skills section

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
 particlesJS.load('particles-js', 'js/vendors/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+$(document).ready(function () {
+  let menuItems = $('.navbar-nav li');
+  menuItems.first().addClass('active');
+
+  menuItems.click((e) => {
+    menuItems.removeClass('active');
+
+    $(e.currentTarget).addClass('active');
+  })
+});
