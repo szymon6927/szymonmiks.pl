c3c8bfb0577cc5e32ea6135604974d093ede9b1f
Szymon Miks <miks.szymon@gmail.com>
2018-08-21 22:59:53 +0200

    complete skills section, add contact section, add back-to-top button, add scroling to section when click in navbar

--- js/script.js
+++ js/script.js
@@ -1,15 +1,50 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'js/vendors/particles.json', function() {
+particlesJS.load('particles-js', 'js/vendors/particles.json', function () {
   console.log('callback - particles.js config loaded');
 });
+
+function goToByScroll(className) {
+  if (!className.includes('.')) {
+    className = "." + className
+  }
+
+  $('html, body').animate({
+    scrollTop: $(className).offset().top - 64
+  }, 1200);
+}
 
 $(document).ready(function () {
   let menuItems = $('.navbar-nav li');
   menuItems.first().addClass('active');
 
   menuItems.click((e) => {
+    e.preventDefault();
     menuItems.removeClass('active');
-
     $(e.currentTarget).addClass('active');
-  })
+    let className = $(e.currentTarget).data('goto');
+    console.log(className);
+    goToByScroll(className);
+  });
+
+  $('.go-to-top').click(() => {
+    $('html, body').animate({scrollTop: 0}, 1500);
+  });
+
+  $('#back-top').hide();
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-top').fadeIn();
+      } else {
+        $('#back-top').fadeOut();
+      }
+    });
+    $('#back-top').click(function () {
+      $('body,html').animate({
+        scrollTop: 0
+      }, 1500);
+      return false;
+    });
+  });
+
 });
